0941efbef78be39a7f889530f96d4a9a8ede5680
Chris McArthur <prince.chrismc@gmail.com>
2021-01-23 12:31:15 -0500

    calling tool-cache to see python versions

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
+    const allPythonVersions = tc.findAllVersions('python')
+    console.log(`Versions of python available: ${allPythonVersions}`)
+
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
